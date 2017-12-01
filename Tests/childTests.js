7ea2515c48c1cbcbbe8c35739cbb794a343cf108
Zachary Williams <wil11063@byui.edu>
2017-12-01 12:18:43 -0700

    Removed console logs

--- Tests/childTests.js
+++ Tests/childTests.js
@@ -1,10 +1,32 @@
 /* Dependencies */
 const tap = require('tap');
 
+
+
 function g1Tests(course, callback) {
     // Tap tests for Gauntlet 3 go here
-    tap.pass('Success! Wheee!');
-    tap.fail('YOLO');
+    function testFileContents(regEx, filename) {
+        var fileToTest = course.content.find(file => {
+            return file.name === filename;
+        });
+        if (fileToTest.ext === '.xml') {
+            return regEx.test(fileToTest.dom.xml());
+        } else {
+            return regEx.test(fileToTest.dom.html());
+        }
+    }
+
+    function checkStoredRefs(refArray, filename) {
+        return course.info[refArray].includes(filename);
+    }
+
+    tap.equals(testFileContents(/i\s*-?\s*learn\s*3\.?0?/ig, 'I-Learn 3.0 References.html'), false);
+    tap.equals(testFileContents(/i\s*-?\s*learn\s*3\.?0?/ig, 'I-learn 3.0 References #2.html'), false);
+    tap.equals(testFileContents(/i\s*-?\s*learn\s*3\.?0?/ig, 'I-learn 3.0 References #3.html'), false);
+    tap.equals(checkStoredRefs('googleHangoutRefs', 'Google Hangout References.html'), true);
+    tap.equals(checkStoredRefs('adobeConnectRefs', 'Adobe Connect References.html'), true);
+    tap.equals(checkStoredRefs('brainhoneyRefs', 'Brainhoney References.html'), true);
+    tap.equals(checkStoredRefs('brightspaceRefs', 'Brightspace References.html'), true);
     callback(null, course);
 }
 
--- main.js
+++ main.js
@@ -7,26 +7,75 @@
 const cheerio = require('cheerio');
 
 module.exports = (course, stepCallback) => {
-    course.addModuleReport('ilearn3References');
+    course.addModuleReport('ilearn-3-references');
 
-    // Go through each file in course.content
-
-    // Do we want to run this on the manifest?
-
-    // Use Regex to replace any references to these:
-    /* I-Learn 3.0, I-Learn 3, iLearn 3.0, ILearn 3.0, ILearn 3, etc. */
-
-    // Change them to just "I-Learn"
+    course.newInfo('brightspaceRefs', []);
+    course.newInfo('brainhoneyRefs', []);
+    course.newInfo('adobeConnectRefs', []);
+    course.newInfo('googleHangoutRefs', []);
 
     course.content.forEach(file => {
-        var html = file.dom('*').html();
-        var reg = /[iI]\s?-?\s?[lL]earn 3\.?0?/g;
-        if (reg.test(html)) {
-            html = html.replace(reg, 'I-Learn');
-            file.dom = cheerio.load(html);
-            console.log(file.dom('*').html());
+        if (file.name === 'imsmanifest.xml' ||
+            file.canEdit === false) {
+            return true;
         }
-    });
+        var guts = '';
+        var changed = false;
+        // RegEx matches and related actions
+        var regs = [
+            {
+                ex: /i\s*-?\s*learn\s*3\.?0?/ig, // I-Learn 3.0 References (and variations)
+                action: () => {
+                    guts = guts.replace(/i\s*-?\s*learn\s*3\.?0?/ig, 'I-Learn');
+                }
+            },
+            {
+                ex: /(brightspace)(?!\.com)/ig, // Brightspace References
+                action: () => {course.info.brightspaceRefs.push(file.name)}
+            },
+            {
+                ex: /brainhoney/ig, // Brainhoney References
+                action: () => {course.info.brainhoneyRefs.push(file.name)}
+            },
+            {
+                ex: /adobe\s*connect/ig, // Adobe Connect References
+                action: () => {course.info.adobeConnectRefs.push(file.name)}
+            },
+            {
+                ex: /((google\s*)?hangouts?(\s*on\s*air)?)|(HOA)/ig, // Google Hangout References
+                action: () => {course.info.googleHangoutRefs.push(file.name)}
+            },
+        ];
 
+        function findRegs(callback) {
+            regs.forEach(regEx => {
+                if (regEx.ex.test(guts)) {
+                    regEx.action();
+                    changed = true;
+                }
+            });
+            callback();
+        }
+
+        // Figure out if it is XML or HTML, then replace I-Learn 3 References
+        if (file.ext === '.xml') {
+            guts = file.dom.xml();
+            findRegs(() => {
+                if (changed) {
+                    course.success('ilearn-3-references', `Changed references/Stored Reference for ${file.name}`);
+                }
+                file.dom = cheerio.load(guts, {xmlMode: true, decodeEntities: false});
+            });
+        } else {
+            guts = file.dom.html();
+            findRegs(() => {
+                if (changed) {
+                    course.success('ilearn-3-references', `Replaced or Stored Reference for ${file.name}`);
+                }
+                file.dom = cheerio.load(guts, {decodeEntities: false});
+            });
+        }
+
+    });
     stepCallback(null, course);
 };
