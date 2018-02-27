960fce1d3638c9dd14e0c361d7d8b1e571932712
Danverde <gre12014@buyi.edu>
2018-02-27 13:19:14 -0700

    removed unnecessary escape characters from regular expressions

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-/*eslint-env node, es6*/
-
 /* Module Description */
-// Removes references to I-learn "3" and makes it say just "I-Learn"
+/* Removes references to I-learn "3" and makes it say just "I-Learn"
 
 /* Put dependencies here */
 const cheerio = require('cheerio');
@@ -25,70 +23,70 @@ module.exports = (course, stepCallback) => {
 
         /* RegEx matches and related actions */
         var regs = [{
-                ex: /i\s*-?\s*learn\s*3\.?0?/ig, // I-Learn 3.0 References (and variations)
-                action: () => {
-                    guts = guts.replace(/i\s*-?\s*learn\s*3\.?0?/ig, 'I-Learn');
-                    course.log('"I-Learn 3.0" Changed to "I-Learn"', {
-                        'File Name': file.name
-                    });
-                }
-            },
-            {
-                ex: /(brightspace)(?!\.com)/ig, // Brightspace References
-                action: () => {
-                    course.log('Contains Brightspace References', {
-                        'File Name': file.name
-                    });
-                }
-            },
-            {
-                ex: /brainhoney/ig, // Brainhoney References
-                action: () => {
-                    course.log('Contains Brainhoney References', {
-                        'File Name': file.name
-                    });
-                }
-            },
-            {
-                ex: /adobe\s*connect/ig, // Adobe Connect References
-                action: () => {
-                    course.log('Contains Adobe Connect References', {
-                        'File Name': file.name
-                    });
-                }
-            },
-            {
-                ex: /((google\s*)?hangouts?(\s*on\s*air)?)|(HOA)/ig, // Google Hangout References
-                action: () => {
-                    course.log('Contains Google Hangouts References', {
-                        'File Name': file.name
-                    });
-                }
-            },
-            {
-                ex: /\<a[^\>]*href=("|')[^"']*\.swf("|')\s*\>/ig, // links to Adobe Flash
-                action: () => {
-                    course.log('Contains Adobe Flash References', {
-                        'File Name': file.name
-                    });
-                }
-            },
-            {
-                ex: /\<!\[CDATA\[[\s\S]*\]\]\>/g, // inline JavaScript
-                action: () => {
-                    course.log('Contains Inline Javascript', {
-                        'File Name': file.name
-                    });
-                }
-            },
-            {
-                ex: /\<style\>/g, // style tags
-                action: () => {
-                    course.log('Contains Inline CSS (Style Tags)', {
-                        'File Name': file.name
-                    });
-                }
+            ex: /i\s*-?\s*learn\s*3\.?0?/ig, // I-Learn 3.0 References (and variations)
+            action: () => {
+                guts = guts.replace(/i\s*-?\s*learn\s*3\.?0?/ig, 'I-Learn');
+                course.log('"I-Learn 3.0" Changed to "I-Learn"', {
+                    'File Name': file.name
+                });
             }
+        },
+        {
+            ex: /(brightspace)(?!\.com)/ig, // Brightspace References
+            action: () => {
+                course.log('Contains Brightspace References', {
+                    'File Name': file.name
+                });
+            }
+        },
+        {
+            ex: /brainhoney/ig, // Brainhoney References
+            action: () => {
+                course.log('Contains Brainhoney References', {
+                    'File Name': file.name
+                });
+            }
+        },
+        {
+            ex: /adobe\s*connect/ig, // Adobe Connect References
+            action: () => {
+                course.log('Contains Adobe Connect References', {
+                    'File Name': file.name
+                });
+            }
+        },
+        {
+            ex: /((google\s*)?hangouts?(\s*on\s*air)?)|(HOA)/ig, // Google Hangout References
+            action: () => {
+                course.log('Contains Google Hangouts References', {
+                    'File Name': file.name
+                });
+            }
+        },
+        {
+            ex: /<a[^>]*href=("|')[^"']*\.swf("|')\s*>/ig, // links to Adobe Flash
+            action: () => {
+                course.log('Contains Adobe Flash References', {
+                    'File Name': file.name
+                });
+            }
+        },
+        {
+            ex: /<!\[CDATA\[[\s\S]*\]\]>/g, // inline JavaScript
+            action: () => {
+                course.log('Contains Inline Javascript', {
+                    'File Name': file.name
+                });
+            }
+        },
+        {
+            ex: /<style>/g, // style tags
+            action: () => {
+                course.log('Contains Inline CSS (Style Tags)', {
+                    'File Name': file.name
+                });
+            }
+        }
         ];
 
         /* Runs the regex tests on the file's contents */
